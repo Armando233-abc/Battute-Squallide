ad547ff0e3eb904d633e27e59ede80325e60eeff
f3lixTTV <giovanniderosa390@gmail.com>
2021-06-01 10:55:09 +0200

    style css

---
+++ src/components/baseLayout.vue
@@ -0,0 +1,55 @@
+<template>
+
+  <ion-page>
+    <ion-header>
+      
+      <ion-toolbar>
+        
+        <ion-buttons slot="start">
+            <ion-back-button default-href="/home"></ion-back-button>
+        </ion-buttons>
+        
+        <ion-title>{{ pageTitle }}</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+       <ion-searchbar>
+ 
+      </ion-searchbar>
+      
+    </ion-content>
+  </ion-page>
+
+</template>
+
+
+<script lang="js">
+import {    
+    IonContent,
+    IonTitle,
+    IonBackButton,
+    IonPage,
+    IonHeader,
+    IonSearchbar,
+    IonToolbar,
+    IonButtons
+} from '@ionic/vue';
+
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'Page',
+  props: ['pageTitle'],
+  components: {
+        IonContent,
+        IonTitle,
+        IonBackButton,
+        IonPage,
+        IonHeader,
+        IonSearchbar,
+        IonToolbar,
+        IonButtons
+  }
+});
+</script>
+
